9ebdeedc47267823536e727da9ce3ebda78dbe3c
orange <jambonbill@gmail.com>
2014-02-23 20:26:15 +0100

    PSR2 changes

--- services/synonyme/index.php
+++ services/synonyme/index.php
@@ -10,76 +10,81 @@ require __DIR__."/../../class.curl.php";
 
 $start = time();
 
-$config = json_decode( file_get_contents( __DIR__ . '/../../config.json') );
+$config = json_decode(file_get_contents(__DIR__.'/../../config.json'));
 
-$cc = new cURL();	
+$cc = new cURL();
 
 $body = @strtolower($_GET['body']);
-if(!$body)die("?");
 
-@preg_match("/synonyme[ :]([a-z]+)/i", $body, $o );
+if (!$body) {
+    die("envois : synonyme {mot}");
+}
+
+@preg_match("/synonyme[ :]([a-z]+)/i", $body, $o);
 
 $mot = $o[1];
-if( !$mot )die("?");
+
+if (!$mot) {
+    die("envois : synonyme {mot}");
+}
 
 $URL = 'http://www.crisco.unicaen.fr/des/synonymes/' . $mot;
 //echo "$URL\n";
 
-$html = $cc->get( $URL );//call the service
+$html = $cc->get($URL);//call the service
 $httpCode = $cc->httpCode();
 $content_type = $cc->contentType();
 $content_length = $cc->contentLength();
 
-$LIST = Array();//list of synonyms
+$LIST = [];//list of synonyms
 
 $detect= false;
 
-if( $httpCode == 200 )
-{
+if ($httpCode == 200) {
 
-	$dat = explode("\n" , $html );
-	foreach( $dat as $k=>$v )
-	{
-		
-		//Start here
-		if( preg_match("/\bClassement des premiers synonymes/i", $v ) ){
-			//echo "<li>Classement des premiers synonymes at line $k<br />";
-			$detect = true;
-		}
-		if( !$detect )continue;
+    $dat = explode("\n", $html);
+    foreach ($dat as $k => $v) {
+        
+        //Start here
+        if (preg_match("/\bClassement des premiers synonymes/i", $v)) {
+            //echo "<li>Classement des premiers synonymes at line $k<br />";
+            $detect = true;
+        }
 
-		//Stop reading here
-		if( preg_match("/\bFin liste10/i" , $v ) ){
-			//echo "<li>fin list detected at line $k<br />";
-			$detect = false;//end
-		}
+        if (!$detect) {
+            continue;
+        }
 
-		//echo "<li>ligne $k<br />";
-		//echo htmlentities( $v );
+        //Stop reading here
+        if (preg_match("/\bFin liste10/i", $v)) {
+            //echo "<li>fin list detected at line $k<br />";
+            $detect = false;//end
+        }
 
-		//Pattern : <a href="/des/synonymes/bannette">&nbsp;bannette&nbsp;</a>
-		preg_match_all("/\ba href=\"\/des\/synonymes\/[a-z]+\">([a-z&;]+)/i", $v , $o );
-		
-		if( is_array( $o ) && count( $o ) )
-		{	
-			if( count( $o[1] ) > 1)$LIST = $o[1];//die();
-		}
-	}
+        //echo "<li>ligne $k<br />";
+        //echo htmlentities( $v );
 
-}
-else
-{
-	//Todo : Log error here
-	echo "Error $httpCode";	
+        //Pattern : <a href="/des/synonymes/bannette">&nbsp;bannette&nbsp;</a>
+        preg_match_all("/\ba href=\"\/des\/synonymes\/[a-z]+\">([a-z&;]+)/i", $v, $o);
+        
+        if (is_array($o) && count($o)) {
+            if (count($o[1])>1) {
+                $LIST = $o[1];//die();
+            }
+        }
+    }
+
+} else {
+    //Todo : Log error here
+    echo "Error $httpCode";
 }
 
 //asort($LIST);
 
 //print_r(array_unique($LIST));
-foreach( $LIST as $k=>$word )
-{
-	$word = str_replace('&nbsp;','',$word);
-	$LIST[$k] = trim(strip_tags( $word ));
+foreach ($LIST as $k => $word) {
+    $word = str_replace('&nbsp;', '', $word);
+    $LIST[$k] = trim(strip_tags($word));
 }
 
-echo implode(", ", $LIST );
+echo implode(", ", $LIST);
